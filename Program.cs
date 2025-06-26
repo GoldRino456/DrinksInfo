5cc08f385ad8a13a9db77ea48f763d1d8daa905b
Ethan E. <eastwoode375@gmail.com>
2025-06-25 20:25:25 -0500

    Test API Call to cocktailDB

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+
+await ProcessRepositoryAsync(client);
+
+static async Task ProcessRepositoryAsync(HttpClient client)
+{
+    var json = await client.GetStringAsync(
+        "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list");
+
+    Console.WriteLine(json);
+}
